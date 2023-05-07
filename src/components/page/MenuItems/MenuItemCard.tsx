fcf7d725d9072b5441900f1b2ce966d715c87a74
Rakesh Baruah <rbaruah@gmail.com>
2023-05-07 13:11:03 -0500

    use rtk query to fetch menu item details page content

--- src/components/page/MenuItems/MenuItemCard.tsx
+++ src/components/page/MenuItems/MenuItemCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { menuItemModel } from "../../../interfaces";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useGetMenuItemByIdQuery } from "../../../apis/menuItemApi";
 
 interface Props {
   menuItem: menuItemModel;
